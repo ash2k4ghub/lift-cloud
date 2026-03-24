97d5ef357780f3819ff7acac8dd1614ea25147b9
ash2k4ghub <uhash2k4@gmail.com>
2026-03-24 13:13:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,47 @@ def update():
 def status():
     return jsonify(lift_data)
 
+@app.route("/recommend")
+def recommend():
+
+    boarding = int(request.args.get("boarding"))
+    destination = int(request.args.get("destination"))
+
+    user_direction = "UP" if destination > boarding else "DOWN"
+
+    best = None
+    best_eta = float("inf")
+
+    for lift in lift_data:
+
+        floor = lift["floor"]
+        eta = lift["eta"]
+
+        # direction logic
+        if floor < boarding:
+            direction = "UP"
+        elif floor > boarding:
+            direction = "DOWN"
+        else:
+            direction = user_direction
+
+        if direction == user_direction and eta < best_eta:
+            best_eta = eta
+            best = lift
+
+    if best:
+        return jsonify({
+            "best_lift": best["name"],
+            "eta": best_eta,
+            "mode": "STRICT"
+        })
+
+    return jsonify({
+        "best_lift": "None",
+        "eta": -1,
+        "mode": "FALLBACK"
+    })
+
 if __name__ == "__main__":
     import os
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
